dc1e1c3030d341dd56d8b74f920efd271e7d6738
Cut0 <zeron9421@gmail.com>
2022-03-08 20:26:17 +0900

    feat: intersectionHooks追加

---
+++ src/features/common/intersectionHooks.ts
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+export const useIntersection = (
+  ref: React.RefObject<HTMLElement>,
+  {
+    enable = true,
+    once = false,
+    threshold = 0,
+    rootMargin = "200px",
+    ...options
+  }: {
+    enable?: boolean;
+    once?: boolean;
+    threshold?: number;
+  } & Omit<IntersectionObserverInit, "threshold">,
+  onIntersection?: () => void,
+  onRemoveIntersection?: () => void,
+) => {
+  useEffect(() => {
+    if (!enable || ref.current === null) {
+      return;
+    }
+    console.log(ref.current);
+
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting && !!onIntersection) {
+        onIntersection();
+      }
+      if (!entry.isIntersecting && !!onRemoveIntersection && !once) {
+        onRemoveIntersection();
+      }
+    }, options);
+
+    observer.observe(ref.current);
+
+    return () => observer.disconnect();
+  }, [onIntersection, onRemoveIntersection, enable, once, options, ref]);
+
+  if (!enable) {
+    return true;
+  }
+};
