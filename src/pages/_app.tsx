db56f9c362785ae0929334389d559eb5ba88a94e
cut0 <zeron9421@gmail.com>
2022-03-20 06:45:07 +0900

    fix: mswの設定削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,6 @@ import "sanitize.css/forms.css";
 import "sanitize.css/typography.css";
 
 import type { AppProps } from "next/app";
-import { initializeMock } from "../../mocks";
-
-if (process.env.NODE_ENV === "development") {
-  initializeMock();
-}
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
