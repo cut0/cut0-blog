919590769f2c6c8513c3e7baf41c6874cc75a2fb
cut0 <zeron9421@gmail.com>
2022-04-04 23:07:50 +0900

    fix: faviconの修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,16 +5,11 @@ class Document extends NextDocument<{}> {
     return (
       <Html lang="ja">
         <Head>
+          <link href="/favicons/favicon.ico" rel="icon" />
           <link
             href="/cut0-blog-ios.png"
             rel="apple-touch-icon"
             sizes="180x180"
-          />
-          <link
-            href="/cut0-blog.png"
-            rel="icon"
-            sizes="192x192"
-            type="image/png"
           />
           {/* <link href="/favicons/site.webmanifest" rel="manifest" /> */}
           {/* <link
@@ -22,8 +17,6 @@ class Document extends NextDocument<{}> {
             href="/favicons/safari-pinned-tab.svg"
             rel="mask-icon"
           /> */}
-          <link href="/favicons/favicon.ico" rel="icon" />
-          <link href="/favicons/favicon.ico" rel="shortcut icon" />
           <meta content="#2B2B2E" name="theme-color" />
         </Head>
         <body>
