27a70ae23ed5894a6856afb74caab2c312388432
cut0 <zeron9421@gmail.com>
2022-09-27 17:06:42 +0900

    :bug: filename 修正

--- src/components/common/ErrorToast.tsx
+++ src/components/common/ErrorToast.tsx
@@ -1,5 +1,5 @@
 import { useEffect, FC } from "react";
-import { CloseSvgIcon } from "../icons/TwitterSvgIcon copy";
+import { CloseSvgIcon } from "../icons/CloseSvgIcon";
 import {
   errorToastContent,
   closeIconContainer,
