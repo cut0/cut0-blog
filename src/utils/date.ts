d1373386ce17439bdab8dced0565564bd5a2e0ec
cut0 <zeron9421@gmail.com>
2022-09-27 16:27:40 +0900

    :sparkles: dayjs やめた

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,13 +1,4 @@
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
-
-export const formatDate = (
-  timeString: string,
-  format: string = "YYYY年M月D日",
-) => {
-  return dayjs.utc(timeString).tz("Asia/Tokyo").format(format);
+export const formatDate = (timeString: string) => {
+  const date = new Date(timeString);
+  return `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日`;
 };
