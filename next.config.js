aab03c07d7a4925cf4f20c0e2dcfe049a3966402
cut0 <zeron9421@gmail.com>
2022-09-27 17:28:48 +0900

    :recycle: 不要な設定削除

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
   images: {
     disableStaticImages: true,
     domains: ["placehold.jp", "images.microcms-assets.io"],
