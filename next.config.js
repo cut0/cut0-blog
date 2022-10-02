7d240f62e51985ac7acd756c5314bc98b8c9ab04
cut0 <zeron9421@gmail.com>
2022-10-02 19:18:26 +0900

    :recycle: スクロール位置の保持

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
 const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
 const withVanillaExtract = createVanillaExtractPlugin();
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    scrollRestoration: true,
+  },
   images: {
     disableStaticImages: true,
     domains: ["placehold.jp", "images.microcms-assets.io"],
   },
 };
 
-module.exports = withVanillaExtract(nextConfig);
+module.exports = withBundleAnalyzer(withVanillaExtract(nextConfig));
