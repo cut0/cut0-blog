491525cec9bdeac662aa4af38c3599e1174ef0a5
cut0 <zeron9421@gmail.com>
2023-12-31 00:20:48 +0900

    feat: Hono 対応

--- workspaces/app/astro.config.ts
+++ workspaces/app/astro.config.ts
@@ -1,9 +1,14 @@
 // import compress from "astro-compress";
 import partytown from "@astrojs/partytown";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default {
   outDir: "../../dist",
+  output: "static",
+  adapter: node({
+    mode: "middleware",
+  }),
   plugins: [],
   integrations: [
     // compress(),
--- workspaces/server/src/index.ts
+++ workspaces/server/src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello");
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+
+const port = 3000;
+
+const app = new Hono();
+
+app.use(
+  "*",
+  serveStatic({
+    root: "../../dist",
+  }),
+);
+
+serve({ fetch: app.fetch, port }, () => {
+  console.log(`slistening on http://localhost:${port}/`);
+});
