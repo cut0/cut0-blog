76e1df050bf622d6191ca38a1b1c69c5af55e1d4
cut0 <zeron9421@gmail.com>
2023-12-31 04:41:35 +0900

    chore: PORT を外から指定できるように

--- workspaces/server/src/index.ts
+++ workspaces/server/src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
 
-const port = 3000;
+const port = parseInt(process.env.PORT ?? "3000");
 
 const app = new Hono();
 
