246bcf872559b7715ab5a7d452f5cbea0cd7a201
cut0 <zeron9421@gmail.com>
2024-01-03 06:15:54 +0900

    chore: ちょっとしゅうせい

--- workspaces/scripts/release.config.js
+++ workspaces/scripts/release.config.js
@@ -23,7 +23,8 @@ module.exports = {
     [
       "@semantic-release/exec",
       {
-        successCmd: "echo HELLO",
+        successCmd:
+          "docker buildx build -f ../../Dockerfile -t ${process.env.IMAGE}:${nextRelease.version} --push ../..",
       },
     ],
     [
