dbe347889824f6eae7329f3467f994bb5f1a75e4
cut0 <zeron9421@gmail.com>
2024-01-03 05:00:29 +0900

    chore: release.config.js 設置

---
+++ workspaces/scripts/release.config.js
@@ -0,0 +1,38 @@
+//eslint-disable-next-line no-undef
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          { breaking: true, release: "major" },
+          { revert: true, release: "patch" },
+          { type: "feat", release: "minor" },
+          { type: "update", release: "minor" },
+          { type: "disable", release: "minor" },
+          { type: "!(release)", release: "patch" },
+        ],
+        parserOpts: {
+          revertPattern: /^Revert\s"([\s\S]*)"$/m,
+          revertCorrespondence: ["header"],
+        },
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        successCmd:
+          "docker buildx build -t ${process.env.IMAGE}:${nextRelease.version} --push .",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        successComment: false,
+        failTitle: false,
+      },
+    ],
+  ],
+};
